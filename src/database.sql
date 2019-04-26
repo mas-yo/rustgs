66d927c6afb1f4f528990ac541d848e7da2c66e6
Masahiro Yoshida <mypianoplayer@gmail.com>
2019-04-26 19:28:20 +0900

    update db schema

--- src/database.sql
+++ src/database.sql
@@ -1,11 +1,9 @@
-DROP TABLE IF EXISTS `join_room_queue`;
 CREATE TABLE `join_room_queue` (
   `user_id` bigint(20) unsigned NOT NULL,
   `room_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `rooms`;
 CREATE TABLE `rooms` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `code` int(11) NOT NULL,
@@ -14,17 +12,16 @@ CREATE TABLE `rooms` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `servers`;
 CREATE TABLE `servers` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `address` varchar(64) NOT NULL,
-  UNIQUE KEY `id` (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `address` (`address`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
+  `name` varchar(256) NOT NULL,
   `room_code` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
